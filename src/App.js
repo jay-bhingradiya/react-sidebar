dc70d74bd9fa666fd0f94040ae40fae0a04a70dd
jay-bhingradiya <jay.bhingradiya@bacancy.com>
2021-08-16 11:51:25 +0530

    sidebar done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SidebarComponent from './components/SidebarComponent';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <SidebarComponent />
+    </div>
+  );
+};
+
+export default App;
